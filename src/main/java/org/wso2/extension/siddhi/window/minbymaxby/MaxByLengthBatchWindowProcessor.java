146ac79fb4ac5245fda160870e0b7a2180dadde4
mathuriga <tmathu24@yahoo.com>
2016-10-05 22:34:54 +0530

    Merge branch 'master' of https://github.com/mathuriga/siddhi-extension-minbymaxby
    # Conflicts:
    #	src/main/java/org/wso2/extension/siddhi/window/minbymaxby/MaxByMinByExecutor.java

--- src/main/java/org/wso2/extension/siddhi/window/minbymaxby/MaxByLengthBatchWindowProcessor.java
+++ src/main/java/org/wso2/extension/siddhi/window/minbymaxby/MaxByLengthBatchWindowProcessor.java
@@ -31,10 +31,10 @@ import org.wso2.siddhi.core.query.processor.Processor;
  * Created by mathuriga on 29/09/16.
  */
 public class MaxByLengthBatchWindowProcessor extends MaxByMinByLengthBatchWindowProcessor {
-    String functionType = "MAX";
+    //String functionType = "MAX";
 
     public MaxByLengthBatchWindowProcessor() {
-        super.functionType = this.functionType;
+        super.minByMaxByExecutorType = "MAX";
     }
 
     protected void init(ExpressionExecutor[] expressionExecutors, ExecutionPlanContext executionPlanContext) {
